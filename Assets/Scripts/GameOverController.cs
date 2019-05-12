ea290a11914b0a1aabd45a92868b845eb67f4974
Lucas Oliveira <lukasooliveira@gmail.com>
2019-05-12 00:49:54 -0300

    GameOver Controller

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverController : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	public float restartDelay = 5f;
+
+	private Animator anim;
+	private float restartTime;
+
+	void Awake () {
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if (playerHealth.health <= 0)
+		{
+			anim.SetTrigger("GameOver");
+			restartTime += Time.deltaTime;
+			if (restartTime >= restartDelay)
+			{
+				//Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+}
